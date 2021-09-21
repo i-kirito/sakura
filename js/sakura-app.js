0c6756822696fca190d706aaad6b94c1304ccf82
ikirito <ikirito@github.com>
2021-09-21 14:21:14 +0000

    增加留言板诗词

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -32,6 +32,22 @@ mashiro_global.ini = new function() {
         checkskinSecter();
     }
 }
+/*诗*/
+function get_poem(poem_ele, info_ele) {
+    var poem = document.querySelector(poem_ele);
+    var info = document.querySelector(info_ele);
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', 'https://v2.jinrishici.com/one.json');
+    xhr.withCredentials = true;
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var data = JSON.parse(xhr.responseText);
+            poem.innerHTML = data.data.content;
+            info.innerHTML = '【' + data.data.origin.dynasty + '】' + data.data.origin.author + '《' + data.data.origin.title + '》';
+        }
+    };
+    xhr.send();
+}
 function setCookie(name, value, days) {
     var expires = "";
     if (days) {
